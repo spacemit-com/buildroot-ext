865393d5fb7b6cbd7f375d4be35839f419f45b0e
max <guoqun.ma@spacemit.com>
2023-11-02 17:41:43 +0800

    change external path's name to Bianbu
    Change-Id: I08e359b63eeb6afd048c1e3b12c2a1d55cb58092

--- external.mk
+++ external.mk
@@ -1,6 +1,6 @@
-include $(sort $(wildcard $(BR2_EXTERNAL_k1_PATH)/package/*/*.mk))
-include $(sort $(wildcard $(BR2_EXTERNAL_k1_PATH)/board/*/*.mk))
-include $(sort $(wildcard $(BR2_EXTERNAL_k1_PATH)/board/*/*/*.mk))
+include $(sort $(wildcard $(BR2_EXTERNAL_Bianbu_PATH)/package/*/*.mk))
+include $(sort $(wildcard $(BR2_EXTERNAL_Bianbu_PATH)/board/*/*.mk))
+include $(sort $(wildcard $(BR2_EXTERNAL_Bianbu_PATH)/board/*/*/*.mk))
 
 
 burn-image:
