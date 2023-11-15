0a26d41a92a453b858c571be53d9937d5d3f1faa
max <guoqun.ma@spacemit.com>
2023-11-15 13:43:23 +0800

    add kernel fit for fpga
    Change-Id: I5bd4d8b54a54380ccc24fd7cb4063beb2fc63321

--- board/spacemit/k1x-fpga/prepare_img.sh
+++ board/spacemit/k1x-fpga/prepare_img.sh
@@ -6,15 +6,13 @@
 
 IMGS_DIR=$1
 CFG_DIR=$(dirname $0)
-#UNSIGN_IMAGES_DIR="$(dirname $1)/unsign-firmware"
-#SIGNED_IMAGES_DIR="$(dirname $1)/signed-firmware"
 OVERRIDE_ROOTFS_FILE="$CFG_DIR/rootfs.ext4"
 FSBL_YML_FILE="$CFG_DIR/fsbl.yml"
-UBOOT_FIT_FILE="$CFG_DIR/uboot-fit.its"
+UBOOT_FIT_FILE="$CFG_DIR/uboot_fit.its"
+KERNEL_FIT_FILE="$CFG_DIR/kernel_fdt.its"
 OPENSBI_FIT_FILE="$CFG_DIR/opensbi-fit.its"
 
 TARGET_ROOTFS_FILE="$IMGS_DIR/rootfs.ext2"
-TARGET_BOOTFS_FILE="$IMGS_DIR/bootfs.img"
 
 #override rootfs.ext4
 if [ -f "$OVERRIDE_ROOTFS_FILE" ]; then
@@ -23,21 +21,25 @@ if [ -f "$OVERRIDE_ROOTFS_FILE" ]; then
 fi
 
 #create header for uboot-spl.bin and rename to FSBL.bin
-rm ${IMGS_DIR}/FSBL.bin
-cp -f ${FSBL_YML_FILE} ${IMGS_DIR}/
-python3 ../scripts/build_binary_file.py -c ${IMGS_DIR}/fsbl.yml -o ${IMGS_DIR}/FSBL.bin
-rm ${IMGS_DIR}/fsbl.yml
+#rm ${IMGS_DIR}/FSBL.bin
+#cp -f ${FSBL_YML_FILE} ${IMGS_DIR}/
+#python3 ../scripts/build_binary_file.py -c ${IMGS_DIR}/fsbl.yml -o ${IMGS_DIR}/FSBL.bin
+#rm ${IMGS_DIR}/fsbl.yml
 
 #copy uboot its file and mk itb
 rm ${IMGS_DIR}/u-boot.itb
-cp -f ${UBOOT_FIT_FILE} ${IMGS_DIR}/uboot-fit.its
-$IMGS_DIR/../host/bin/mkimage -f ${IMGS_DIR}/uboot-fit.its -r ${IMGS_DIR}/u-boot.itb
-rm ${IMGS_DIR}/uboot-fit.its
+cp -f ${UBOOT_FIT_FILE} ${IMGS_DIR}/uboot_fit.its
+$IMGS_DIR/../host/bin/mkimage -f ${IMGS_DIR}/uboot_fit.its -r ${IMGS_DIR}/u-boot.itb
+rm ${IMGS_DIR}/uboot_fit.its
 
 #copy opensbi its file and mk itb
-rm ${IMGS_DIR}/opensbi.itb
-cp -f ${OPENSBI_FIT_FILE} ${IMGS_DIR}/opensbi-fit.its
-$IMGS_DIR/../host/bin/mkimage -f ${IMGS_DIR}/opensbi-fit.its -r ${IMGS_DIR}/opensbi.itb
-rm ${IMGS_DIR}/opensbi-fit.its
+#rm ${IMGS_DIR}/opensbi.itb
+#cp -f ${OPENSBI_FIT_FILE} ${IMGS_DIR}/opensbi-fit.its
+#$IMGS_DIR/../host/bin/mkimage -f ${IMGS_DIR}/opensbi-fit.its -r ${IMGS_DIR}/opensbi.itb
+#rm ${IMGS_DIR}/opensbi-fit.its
 
 
+rm ${IMGS_DIR}/uImage.itb
+cp -f ${UBOOT_FIT_FILE} ${IMGS_DIR}/kernel_fdt.its
+$IMGS_DIR/../host/bin/mkimage -f ${IMGS_DIR}/kernel_fdt.its -r ${IMGS_DIR}/uImage.itb
+rm ${IMGS_DIR}/kernel_fdt.its
