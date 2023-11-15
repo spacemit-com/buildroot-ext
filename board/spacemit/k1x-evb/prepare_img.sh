819d8c2cf57c10ced4dcb7cf6f6c624a28eb15e6
max <guoqun.ma@spacemit.com>
2023-11-15 14:51:04 +0800

    use the auto generated cfg to pack img
    Change-Id: I2e792e1b44b5364cb15135fbc422df2c347491d2

--- board/spacemit/k1x-evb/prepare_img.sh
+++ board/spacemit/k1x-evb/prepare_img.sh
@@ -12,6 +12,8 @@ OVERRIDE_ROOTFS_FILE="$CFG_DIR/rootfs.ext4"
 FSBL_YML_FILE="$CFG_DIR/fsbl.yml"
 UBOOT_FIT_FILE="$CFG_DIR/uboot_fit.its"
 OPENSBI_FIT_FILE="$CFG_DIR/opensbi_fit.its"
+PARTITIONS_FILE="$CFG_DIR/partitions.json"
+GENIMAGE_CFG_FILE="$CFG_DIR/sd-genimage.cfg"
 
 TARGET_ROOTFS_FILE="$IMGS_DIR/rootfs.ext2"
 TARGET_BOOTFS_FILE="$IMGS_DIR/bootfs.img"
@@ -40,4 +42,6 @@ cp -f ${OPENSBI_FIT_FILE} ${IMGS_DIR}/opensbi_fit.its
 $IMGS_DIR/../host/bin/mkimage -f ${IMGS_DIR}/opensbi_fit.its -r ${IMGS_DIR}/opensbi.itb
 rm ${IMGS_DIR}/opensbi_fit.its
 
-
+#update sd-geimage.cfg
+../scripts/gen_imgcfg.py  ${PARTITIONS_FILE}
+mv ./genimage.cfg ${GENIMAGE_CFG_FILE}
