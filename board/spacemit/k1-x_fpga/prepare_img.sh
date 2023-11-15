38a3c5f28ea891e82aeb67d844b658f0a308fe1c
max <guoqun.ma@spacemit.com>
2023-11-15 11:10:30 +0800

    optz vars define for prepare_img.sh
    Change-Id: I9a2be0c7d0b31e9445ab9284aa24cdb179d2c353

--- board/spacemit/k1-x_fpga/prepare_img.sh
+++ board/spacemit/k1-x_fpga/prepare_img.sh
@@ -2,18 +2,19 @@
 
 ######################## prepare img to be packed ####################
 #$0 is this file path
-#$1 is buildroot output target dir
+#$1 is buildroot output images dir
 
-BUILDROOT_IMAGES_DIR="$(dirname $1)/images"
+IMGS_DIR=$1
+CFG_DIR=$(dirname $0)
 #UNSIGN_IMAGES_DIR="$(dirname $1)/unsign-firmware"
 #SIGNED_IMAGES_DIR="$(dirname $1)/signed-firmware"
-OVERRIDE_ROOTFS_FILE="$(dirname $0)/rootfs.ext4"
-FSBL_YML_FILE="$(dirname $0)/fsbl.yml"
-UBOOT_FIT_FILE="$(dirname $0)/uboot-fit.its"
-OPENSBI_FIT_FILE="$(dirname $0)/opensbi-fit.its"
+OVERRIDE_ROOTFS_FILE="$CFG_DIR/rootfs.ext4"
+FSBL_YML_FILE="$CFG_DIR/fsbl.yml"
+UBOOT_FIT_FILE="$CFG_DIR/uboot-fit.its"
+OPENSBI_FIT_FILE="$CFG_DIR/opensbi-fit.its"
 
-TARGET_ROOTFS_FILE="$BUILDROOT_IMAGES_DIR/rootfs.ext2"
-TARGET_BOOTFS_FILE="$BUILDROOT_IMAGES_DIR/bootfs.img"
+TARGET_ROOTFS_FILE="$IMGS_DIR/rootfs.ext2"
+TARGET_BOOTFS_FILE="$IMGS_DIR/bootfs.img"
 
 #override rootfs.ext4
 if [ -f "$OVERRIDE_ROOTFS_FILE" ]; then
@@ -22,28 +23,21 @@ if [ -f "$OVERRIDE_ROOTFS_FILE" ]; then
 fi
 
 #create header for uboot-spl.bin and rename to FSBL.bin
-rm ${BUILDROOT_IMAGES_DIR}/FSBL.bin
-cp -f ${FSBL_YML_FILE} ${BUILDROOT_IMAGES_DIR}/
-python3 ../scripts/build_binary_file.py -c ${BUILDROOT_IMAGES_DIR}/fsbl.yml -o ${BUILDROOT_IMAGES_DIR}/FSBL.bin
-rm ${BUILDROOT_IMAGES_DIR}/fsbl.yml
+rm ${IMGS_DIR}/FSBL.bin
+cp -f ${FSBL_YML_FILE} ${IMGS_DIR}/
+python3 ../scripts/build_binary_file.py -c ${IMGS_DIR}/fsbl.yml -o ${IMGS_DIR}/FSBL.bin
+rm ${IMGS_DIR}/fsbl.yml
 
 #copy uboot its file and mk itb
-rm ${BUILDROOT_IMAGES_DIR}/uboot-fit.itb
-cp -f ${UBOOT_FIT_FILE} ${BUILDROOT_IMAGES_DIR}/uboot-fit.its
-mkimage -f ${BUILDROOT_IMAGES_DIR}/uboot-fit.its -r ${BUILDROOT_IMAGES_DIR}/uboot-fit.itb
-rm ${BUILDROOT_IMAGES_DIR}/uboot-fit.its
+rm ${IMGS_DIR}/u-boot.itb
+cp -f ${UBOOT_FIT_FILE} ${IMGS_DIR}/uboot-fit.its
+$IMGS_DIR/../host/bin/mkimage -f ${IMGS_DIR}/uboot-fit.its -r ${IMGS_DIR}/u-boot.itb
+rm ${IMGS_DIR}/uboot-fit.its
 
 #copy opensbi its file and mk itb
-rm ${BUILDROOT_IMAGES_DIR}/opensbi-fit.itb
-cp -f ${OPENSBI_FIT_FILE} ${BUILDROOT_IMAGES_DIR}/opensbi-fit.its
-mkimage -f ${BUILDROOT_IMAGES_DIR}/opensbi-fit.its -r ${BUILDROOT_IMAGES_DIR}/opensbi-fit.itb
-rm ${BUILDROOT_IMAGES_DIR}/opensbi-fit.its
-
-
-#copy bootfs img
-#ln -s ${TARGET_BOOTFS_FILE}  ${UNSIGN_IMAGES_DIR}/bootfs.img
-
-#copy rootfs img
-#ln -s ${TARGET_ROOTFS_FILE}  ${UNSIGN_IMAGES_DIR}/rootfs.ext4
+rm ${IMGS_DIR}/opensbi.itb
+cp -f ${OPENSBI_FIT_FILE} ${IMGS_DIR}/opensbi-fit.its
+$IMGS_DIR/../host/bin/mkimage -f ${IMGS_DIR}/opensbi-fit.its -r ${IMGS_DIR}/opensbi.itb
+rm ${IMGS_DIR}/opensbi-fit.its
 
 
