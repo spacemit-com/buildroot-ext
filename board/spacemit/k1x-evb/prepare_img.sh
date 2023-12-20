114ada9ed2f09f0b8a350b710bb8364a758c08e0
max <guoqun.ma@spacemit.com>
2023-12-20 14:34:22 +0800

    add fastboot flash script for linux and win
    Change-Id: I330057876cadcbab6fc5d7ab3662af53faf44dfe

--- board/spacemit/k1x-evb/prepare_img.sh
+++ board/spacemit/k1x-evb/prepare_img.sh
@@ -155,6 +155,10 @@ pack_image_zip() {
     echo -e "\n"
 }
 
+cp_flash_scripts() {
+    cp -f ${DEVICE_DIR}/fastboot_load.sh ${IMGS_DIR}/
+    cp -f ${DEVICE_DIR}/fastboot_load.bat ${IMGS_DIR}/
+}
 #FSBL opensbi uboot uImage
 gen_sub_images
 
@@ -170,4 +174,5 @@ pack_image_zip
 #gen sdcard.img if need
 gen_sdcard_img
 
+cp_flash_scripts
 
