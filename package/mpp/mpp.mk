e49593d9f9abe19c598e5995c2726847c1ae10da
fuqiang <qiang.fu@spacemit.com>
2023-12-27 15:39:17 +0800

    some optimizes, include :
    1.enable FFMPEG_SPACEMIT_CODEC, depends on FFMPEG_GPL, select MPP
    2.copy some .h and .so to STAGING_DIR, used for ffmpeg compiling
    
    Change-Id: I82075c15c64df61ed853415fd9320e8a176d7fca
    Signed-off-by: fuqiang <qiang.fu@spacemit.com>

--- package/mpp/mpp.mk
+++ package/mpp/mpp.mk
@@ -2,6 +2,7 @@ MPP_VERSION := 0.1.0
 MPP_SITE = $(TOPDIR)/../package-src/mpp
 MPP_SITE_METHOD = local
 MPP_SUPPORTS_IN_SOURCE_BUILD = NO
+MPP_INSTALL_STAGING = YES
 #MPP_DEPENDENCIES = jpeg libv4l libdrm
 
 # default CMAKE_INSTALL_PREFIX is $(TARGET_DIR)/usr, change to below
@@ -10,5 +11,13 @@ MPP_CONF_OPTS = -DRUN_PLATFORM="RISCV" \
 			-DCI_LOG_LEVEL=4 \
 			-DARCH_RISCV="Y" \
 			-DCMAKE_INSTALL_PREFIX="/usr" \
+
+define MPP_INSTALL_STAGING_CMDS
+	cp -rdpf $(@D)/include/* $(STAGING_DIR)/usr/include/
+	cp -rdpf $(@D)/mpi/include/* $(STAGING_DIR)/usr/include/
+	cp -rdpf $(@D)/utils/include/* $(STAGING_DIR)/usr/include/
+	cp -rdpf $(@D)/al/include/* $(STAGING_DIR)/usr/include/
+	cp -rdpf $(@D)/buildroot-build/mpi/libspacemit_mpp.so* $(STAGING_DIR)/usr/lib/
+endef
 
 $(eval $(cmake-package))
