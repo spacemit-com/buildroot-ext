b8170218ba8dcbbe70e78b61a79edb5aaa6c2e17
max <guoqun.ma@spacemit.com>
2023-12-30 11:32:51 +0800

    prepare_img.sh add return value checking
    Change-Id: I8768fd614ad71b76cd0544254b1f9e5a22fb82f7

--- board/spacemit/k1x-evb/prepare_img.sh
+++ board/spacemit/k1x-evb/prepare_img.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-######################## prepare sub img to be packed ####################
+######################## Prepare sub-iamges and pack ####################
 #$0 is this file path
 #$1 is buildroot output images dir
 
@@ -14,7 +14,7 @@ PARTITIONS_FILE="$DEVICE_DIR/partition_universal.json"
 GENIMAGE_CFG_FILE="$DEVICE_DIR/sd-genimage.cfg"
 UENV_TXT_FILE="$DEVICE_DIR/env_k1-x.txt"
 
-#give a chance for ci
+#Give a chance to CI
 if [ -z "$BIANBU_LINUX_ARCHIVE" ]; then
     TARGET_IMAGE_ZIP="$IMGS_DIR/spacemit_bianbu_linux_k1-x_evb_emmc_pack_image.zip"
 else
@@ -23,7 +23,7 @@ fi
 
 TARGET_ROOTFS_FILE="$IMGS_DIR/rootfs.ext2"
 TARGET_BOOTFS_FILE="$IMGS_DIR/bootfs.img"
-TARGET_INITRAMFS_FILE=("$IMGS_DIR/rootfs.cpio.*")
+TARGET_INITRAMFS_FILE=("$IMGS_DIR/rootfs.cpio.gz")
 
 BOOTFS_SIZE=$($IMGS_DIR/../host/bin/jq '.partitions[] | select(.name == "bootfs") | .size' "$PARTITIONS_FILE")
 BOOTFS_DIR="$IMGS_DIR/bootfs"
@@ -36,10 +36,10 @@ KERNEL_IMAGE_FILE="$IMGS_DIR/uImage.itb"
 
 FAKE_ROOT_FILE=/tmp/$(whoami)-fakeroot
 
-#pack kernel Image and initramfs
+#Pack kernel image and initramfs
 gen_bootfs_vfat() {
     echo -e "\n"
-    echo "Start to make bootfs ..............................."
+    echo "Starting to build bootfs ..............................."
 
     echo "#!/bin/sh" > "$FAKE_ROOT_FILE"
     echo "set -e" >> "$FAKE_ROOT_FILE"
@@ -58,7 +58,12 @@ gen_bootfs_vfat() {
 
     chmod 777 "$FAKE_ROOT_FILE"
     FAKEROOTDONTTRYCHOWN=1 "$IMGS_DIR/../host/bin/fakeroot" -- "$FAKE_ROOT_FILE"
-    echo "Making bootfs success..............................."
+
+    if [ $? -ne 0 ]; then
+        echo "Building bootfs failed. Please check for errors..............."
+        exit 1
+    fi
+    echo "Bootfs build successful................................."
     echo -e "\n"
 }
 
@@ -66,7 +71,7 @@ override_rootfs_img() {
     #override rootfs.ext4 if needed
     if [ -f "$SRC_ROOTFS_FILE" ]; then
         cp -rf ${SRC_ROOTFS_FILE} ${TARGET_ROOTFS_FILE}
-        echo "override ${TARGET_ROOTFS_FILE} with ${SRC_ROOTFS_FILE} success"
+        echo "Successfully overridden ${TARGET_ROOTFS_FILE} with ${SRC_ROOTFS_FILE}"
     fi
 }
 
@@ -78,7 +83,7 @@ gen_sub_images() {
     #rm ${IMGS_DIR}/fsbl.yml
     #cp -f ${DEVICE_DIR}/bootinfo_sd.bin ${IMGS_DIR}/
 
-    #create header for uboot-spl.bin and rename to FSBL.bin
+    #Create header for uboot-spl.bin and rename to FSBL.bin
     rm -f ${IMGS_DIR}/FSBL.bin
     cp -f ${FSBL_YML_FILE} ${IMGS_DIR}/
     python3 $PWD/../scripts/build_binary_file.py -c ${IMGS_DIR}/fsbl.yml -o ${IMGS_DIR}/FSBL.bin
@@ -95,10 +100,10 @@ gen_sub_images() {
     $IMGS_DIR/../host/bin/mkimage -f ${IMGS_DIR}/uboot-opensbi.its -r ${IMGS_DIR}/u-boot-opensbi.itb
     rm ${IMGS_DIR}/uboot-opensbi.its
     
-    #rename to opensbi.itb for partition file need
+    #Rename to opensbi.itb for the partition file
     cp -f ${IMGS_DIR}/fw_dynamic.itb ${IMGS_DIR}/opensbi.itb
 
-    #maybe gen kernel Image dtb here
+    #Gen kernel Image dtb here
     rm -f ${IMGS_DIR}/uImage.itb
     cp -f ${KERNEL_FIT_FILE} ${IMGS_DIR}/kernel_fdt.its
     $IMGS_DIR/../host/bin/mkimage -f ${IMGS_DIR}/kernel_fdt.its -r ${IMGS_DIR}/uImage.itb
@@ -107,19 +112,24 @@ gen_sub_images() {
 }
 
 update_genimage_cfg() {
-    #update sd-geimage.cfg
+    #Update sd-geimage.cfg
     $PWD/../scripts/gen_imgcfg.py  ${PARTITIONS_FILE}
     mv $PWD/./genimage.cfg ${GENIMAGE_CFG_FILE}
     cp -f ${GENIMAGE_CFG_FILE}  ${IMGS_DIR}/genimage.cfg
 }
 
 gen_sdcard_img() {
-    echo "Generating sdcard.img..............................."
+    echo "Generating sdcard.img..................................."
     $PWD/support/scripts/genimage.sh -c ${IMGS_DIR}/genimage.cfg
+    if [ $? -ne 0 ]; then
+        echo "Generating bootfs failed. Please check for errors..............."
+        exit 1
+    fi
+    echo "sdcard.img successfully generated at ${IMGS_DIR}/sdcard.img"
 }
 
 pack_image_zip() {
-    echo "Start to pack images................................"
+    echo "Starting to pack images................................."
     rm -f ${TARGET_IMAGE_ZIP}
     rm -rf ${IMGS_DIR}/factory
 
@@ -141,7 +151,7 @@ pack_image_zip() {
         genimage.cfg \
         -r factory
 
-    #give a chance for CI
+    #Give a chance to CI
     if [ -n "$BIANBU_LINUX_ARCHIVE_LATEST" ]; then
         ln -sf ${TARGET_IMAGE_ZIP} $BIANBU_LINUX_ARCHIVE_LATEST
     fi
@@ -151,7 +161,7 @@ pack_image_zip() {
     #    partition_universal.json \
     cd - >/dev/null
  
-    echo "Success to pack images into ${TARGET_IMAGE_ZIP}"
+    echo "Images successfully packed into ${TARGET_IMAGE_ZIP}"
     echo -e "\n"
 }
 
@@ -163,19 +173,19 @@ cp_flash_scripts() {
 #FSBL opensbi uboot uImage
 gen_sub_images
 
-#gen bootfs
+#Gen bootfs
 gen_bootfs_vfat
 
-#for Debian or Ubuntu rootfs override
+#For Debian or Ubuntu rootfs override
 override_rootfs_img
 
-#update genimage cfg, because pack_image_zip depend on it
+#Update genimage cfg because pack_image_zip dependis on it
 update_genimage_cfg
 
-#pack image in zip
+#Pack images in zip
 pack_image_zip
 
-#gen sdcard.img if need
+#Gen sdcard.img if need
 gen_sdcard_img
 
 cp_flash_scripts
