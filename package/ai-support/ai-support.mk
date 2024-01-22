3096b620a011f41aca7f420db3b79230c9e55cb4
max <guoqun.ma@spacemit.com>
2024-01-22 19:11:12 +0800

    ai: add data and models
    Change-Id: Ibe1252acb6f10bfe7e5953fd83f9a7b87b049313

--- package/ai-support/ai-support.mk
+++ package/ai-support/ai-support.mk
@@ -7,7 +7,20 @@ AI_SUPPORT_CONF_OPTS = -DCMAKE_BUILD_TYPE=Debug  \
                         -DTEST=OFF \
                         -DDEMO=ON \
                         -DORT_HOME=$(STAGING_DIR)/lib 
-                        
+
+define AI_SUPPORT_POST_BUILD
+    mkdir -p $(TARGET_DIR)/usr/share/ai-support/config
+    mkdir -p $(TARGET_DIR)/usr/share/ai-support/imgs
+    mkdir -p $(TARGET_DIR)/usr/share/ai-support/labels
+    mkdir -p $(TARGET_DIR)/usr/share/ai-support/models
+    
+    $(INSTALL) -D -m 0644 $(@D)/data/config/* $(TARGET_DIR)/usr/share/ai-support/config/
+    $(INSTALL) -D -m 0644 $(@D)/data/imgs/* $(TARGET_DIR)/usr/share/ai-support/imgs/
+    $(INSTALL) -D -m 0644 $(@D)/data/labels/* $(TARGET_DIR)/usr/share/ai-support/labels/
+    $(INSTALL) -D -m 0644 $(@D)/share/models/* $(TARGET_DIR)/usr/share/ai-support/models/
+
+endef
+AI_SUPPORT_POST_BUILD_HOOKS += AI_SUPPORT_POST_BUILD
 
 
 $(eval $(cmake-package))
