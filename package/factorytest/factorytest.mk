639f31ae92a711053877481c7d0a57b1f222d40a
James Deng <james.deng@spacemit.com>
2024-04-26 19:00:59 +0800

    factorytest: Installed specified files
    Change-Id: Ie13473ab4c305ae5250e8fe2f1f30b7d62547a2a

--- package/factorytest/factorytest.mk
+++ package/factorytest/factorytest.mk
@@ -4,7 +4,12 @@ FACTORYTEST_SITE = local
 
 define FACTORYTEST_INSTALL_TARGET_CMDS
 	rm -rf $(TARGET_DIR)/opt/factorytest
-	cp -r $(@D) $(TARGET_DIR)/opt/factorytest
+	mkdir -p $(TARGET_DIR)/opt/factorytest
+	cp -r $(@D)/cricket $(TARGET_DIR)/opt/factorytest
+	cp -r $(@D)/res $(TARGET_DIR)/opt/factorytest
+	cp -r $(@D)/tests $(TARGET_DIR)/opt/factorytest
+	cp -r $(@D)/utils $(TARGET_DIR)/opt/factorytest
+	cp $(@D)/gui-main $(TARGET_DIR)/opt/factorytest
 endef
 
 $(eval $(generic-package))
