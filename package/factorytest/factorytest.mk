ab9e0f29857f6a1ae4984270a655a2ba558ba1de
James Deng <james.deng@spacemit.com>
2024-04-30 10:31:17 +0800

    factorytest: Installed stability to target
    Change-Id: Ib1e1ac60a6acce573feab1ae1fae062d9cd21671

--- package/factorytest/factorytest.mk
+++ package/factorytest/factorytest.mk
@@ -10,6 +10,9 @@ define FACTORYTEST_INSTALL_TARGET_CMDS
 	cp -r $(@D)/tests $(TARGET_DIR)/opt/factorytest
 	cp -r $(@D)/utils $(TARGET_DIR)/opt/factorytest
 	cp $(@D)/gui-main $(TARGET_DIR)/opt/factorytest
+	cp $(@D)/stability $(TARGET_DIR)/opt/factorytest
+	cp $(@D)/memtester.sh $(TARGET_DIR)/opt/factorytest
+	cp $(@D)/stress-ng.sh $(TARGET_DIR)/opt/factorytest
 endef
 
 $(eval $(generic-package))
