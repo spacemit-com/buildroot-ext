899963db2ca7c6293b8bd6b43f39485916dd093f
James Deng <james.deng@spacemit.com>
2024-06-08 20:28:03 +0800

    board:spacemit:k1: Copy run_weston.sh from factorytest
    Change-Id: I2a834db863407b28eb48649689345f376c61573d

--- board/spacemit/k1/plt_custom.sh
+++ board/spacemit/k1/plt_custom.sh
@@ -1,3 +1,8 @@
 #!/bin/bash -e
 
 cp -r $BR2_EXTERNAL_Bianbu_PATH/board/spacemit/k1/plt_overlay/* $TARGET_DIR
+
+if [ -f $BR2_EXTERNAL_Bianbu_PATH/../package-src/factorytest/run_weston.sh ]
+then
+	cp $BR2_EXTERNAL_Bianbu_PATH/../package-src/factorytest/run_weston.sh $TARGET_DIR/root
+fi
