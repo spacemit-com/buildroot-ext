99754c19ce923462201cb07fc8f4edac391004f3
James Deng <james.deng@spacemit.com>
2024-06-13 20:42:42 +0800

    board:spacemit:k1: Added version for plt
    Change-Id: I701bb2c57995865474b8a3e8548c55881524c776

--- board/spacemit/k1/plt_custom.sh
+++ board/spacemit/k1/plt_custom.sh
@@ -6,3 +6,6 @@ if [ -f $BR2_EXTERNAL_Bianbu_PATH/../package-src/factorytest/run_weston.sh ]
 then
 	cp $BR2_EXTERNAL_Bianbu_PATH/../package-src/factorytest/run_weston.sh $TARGET_DIR/root
 fi
+
+DATE=$(date +%Y%m%d%H%M%S)
+echo "$DATE" | tee "${TARGET_DIR}/etc/bianbu_version"
