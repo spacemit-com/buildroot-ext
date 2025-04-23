91be0775905fd81492f4b89f7a1a3bf968b965af
lilijun <lijun.li@spacemit.com>
2025-04-23 20:55:35 +0800

    add v2d yuv2rgb case
    Change-Id: I7afc293575b00b3000b1efe5dc710223b54463f1

--- package/v2d-test/v2d-test.mk
+++ package/v2d-test/v2d-test.mk
@@ -8,6 +8,8 @@ define V2D_TEST_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/libv2d.so $(TARGET_DIR)/usr/lib/libv2d.so
 	$(INSTALL) -D -m 0755 $(@D)/libdmabufheap.so $(TARGET_DIR)/usr/lib/libdmabufheap.so
 	$(INSTALL) -D -m 0755 $(@D)/res/320_240_bt601_n.yuv $(TARGET_DIR)/usr/share/v2d/320_240_bt601_n.yuv
+	$(INSTALL) -D -m 0755 $(@D)/res/1920_1080_bt601_nv12.yuv $(TARGET_DIR)/usr/share/v2d/1920_1080_bt601_nv12.yuv
+	$(INSTALL) -D -m 0755 $(@D)/res/1920_1080_rgba8888.rgb $(TARGET_DIR)/usr/share/v2d/1920_1080_rgba8888.rgb
 	$(INSTALL) -D -m 0755 $(@D)/res/320_240_yuv_s0_header.fbc $(TARGET_DIR)/usr/share/v2d/320_240_yuv_s0_header.fbc
 	$(INSTALL) -D -m 0755 $(@D)/res/320_240_yuv_s0_payload.fbc $(TARGET_DIR)/usr/share/v2d/320_240_yuv_s0_payload.fbc
 	$(INSTALL) -D -m 0755 $(@D)/res/adv_320_240_rgb888.raw $(TARGET_DIR)/usr/share/v2d/adv_320_240_rgb888.raw
