2f02c8c167ea6ba8471d1b87451e77dca1717db1
lilijun <lijun.li@spacemit.com>
2025-06-12 11:38:10 +0800

    drm test cases and v2d test cases support building deb packages
    Change-Id: I4f7b2c49422b4b65d52f3a64875c62789e45b208

--- package/drm-test/drm-test.mk
+++ package/drm-test/drm-test.mk
@@ -3,20 +3,5 @@ DRM_TEST_SITE = $(BR2_EXTERNAL_Bianbu_PATH)/../package-src/drm-test
 DRM_TEST_SITE_METHOD = local
 DRM_TEST_INSTALL_TARGET = YES
 
-define DRM_TEST_BUILD_CMDS
-	($(TARGET_MAKE_ENV) $(MAKE) CC="$(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_LDFLAGS)" -C $(@D) all;)
-endef
-
-define DRM_TEST_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/res/1920_1080_nv12.yuv $(TARGET_DIR)/usr/share/drm-test/1920_1080_nv12.yuv
-	$(INSTALL) -D -m 0755 $(@D)/modeset-single-buffer $(TARGET_DIR)/usr/bin/
-	$(INSTALL) -D -m 0755 $(@D)/modeset-double-buffer $(TARGET_DIR)/usr/bin/
-	$(INSTALL) -D -m 0755 $(@D)/modeset-vsync $(TARGET_DIR)/usr/bin/
-	$(INSTALL) -D -m 0755 $(@D)/modeset-atomic $(TARGET_DIR)/usr/bin/
-	$(INSTALL) -D -m 0755 $(@D)/modeset-dmabuf $(TARGET_DIR)/usr/bin/
-
-
-endef
-
 DRM_TEST_DEPENDENCIES = libdrm
-$(eval $(generic-package))
+$(eval $(cmake-package))
--- package/v2d-test/v2d-test.mk
+++ package/v2d-test/v2d-test.mk
@@ -3,18 +3,4 @@ V2D_TEST_SITE = $(BR2_EXTERNAL_Bianbu_PATH)/../package-src/v2d-test
 V2D_TEST_SITE_METHOD = local
 V2D_TEST_INSTALL_TARGET = YES
 
-define V2D_TEST_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/v2d_test $(TARGET_DIR)/usr/bin/v2d_test
-	$(INSTALL) -D -m 0755 $(@D)/libv2d.so $(TARGET_DIR)/usr/lib/libv2d.so
-	$(INSTALL) -D -m 0755 $(@D)/libdmabufheap.so $(TARGET_DIR)/usr/lib/libdmabufheap.so
-	$(INSTALL) -D -m 0755 $(@D)/res/320_240_bt601_n.yuv $(TARGET_DIR)/usr/share/v2d/320_240_bt601_n.yuv
-	$(INSTALL) -D -m 0755 $(@D)/res/1920_1080_bt601_nv12.yuv $(TARGET_DIR)/usr/share/v2d/1920_1080_bt601_nv12.yuv
-	$(INSTALL) -D -m 0755 $(@D)/res/1920_1080_rgba8888.rgb $(TARGET_DIR)/usr/share/v2d/1920_1080_rgba8888.rgb
-	$(INSTALL) -D -m 0755 $(@D)/res/320_240_yuv_s0_header.fbc $(TARGET_DIR)/usr/share/v2d/320_240_yuv_s0_header.fbc
-	$(INSTALL) -D -m 0755 $(@D)/res/320_240_yuv_s0_payload.fbc $(TARGET_DIR)/usr/share/v2d/320_240_yuv_s0_payload.fbc
-	$(INSTALL) -D -m 0755 $(@D)/res/adv_320_240_rgb888.raw $(TARGET_DIR)/usr/share/v2d/adv_320_240_rgb888.raw
-	$(INSTALL) -D -m 0755 $(@D)/res/adv_320_240_rgb888_s1_header.fbc $(TARGET_DIR)/usr/share/v2d/adv_320_240_rgb888_s1_header.fbc
-	$(INSTALL) -D -m 0755 $(@D)/res/adv_320_240_rgb888_s1_payload.fbc $(TARGET_DIR)/usr/share/v2d/adv_320_240_rgb888_s1_payload.fbc
-endef
-
 $(eval $(cmake-package))
