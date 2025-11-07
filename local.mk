711ab95f725f8a57d6a34cf37a88ca216c48d037
liangbaihui <baihui.liang@spacemit.com>
2025-11-07 09:22:13 +0800

    chore: update Mesa3D source directory path
    - Rename MESA3D_OVERRIDE_SRCDIR variable from "mesa3d" to "mesa" to align with the actual directory name.
    
    Change-Id: I4085f9542010722ed2a72275491fb023157a9dc6

--- local.mk
+++ local.mk
@@ -10,7 +10,7 @@
 # LINUX_OVERRIDE_SRCDIR = $(TOPDIR)/../bsp-src/linux-6.1
 UBOOT_OVERRIDE_SRCDIR = $(TOPDIR)/../bsp-src/uboot-2022.10
 OPENSBI_OVERRIDE_SRCDIR = $(TOPDIR)/../bsp-src/opensbi
-MESA3D_OVERRIDE_SRCDIR = $(TOPDIR)/../package-src/mesa3d
+MESA3D_OVERRIDE_SRCDIR = $(TOPDIR)/../package-src/mesa
 IMG_GPU_POWERVR_OVERRIDE_SRCDIR = $(TOPDIR)/../package-src/img-gpu-powervr
 GLMARK2_OVERRIDE_SRCDIR = $(TOPDIR)/../package-src/glmark2
 FACTORYTEST_OVERRIDE_SRCDIR = $(TOPDIR)/../package-src/factorytest
