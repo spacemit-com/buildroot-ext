34a31b21f6e800334c39eebe24cdc17c177ded36
liujing <jing.liu@spacemit.com>
2026-01-12 17:38:10 +0800

    factorytest:initial k3-deb1 factorytest with linux-6.12
    Change-Id: I4f6452303dacd3355737001a48f86e6c5318210c

---
+++ board/spacemit/k3/plt_custom.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+cp -r $BR2_EXTERNAL_Bianbu_PATH/board/spacemit/k3/plt_overlay/* $TARGET_DIR
+
+if [ -f $BR2_EXTERNAL_Bianbu_PATH/../package-src/factorytest/run_weston.sh ]
+then
+	cp $BR2_EXTERNAL_Bianbu_PATH/../package-src/factorytest/run_weston.sh $TARGET_DIR/root
+fi
+
+DATE=$(date +%Y%m%d%H%M%S)
+echo "$DATE" | tee "${TARGET_DIR}/etc/bianbu_version"
