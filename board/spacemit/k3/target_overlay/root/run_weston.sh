8e052a29192752a329ec8a36bbef1f5515b21028
liujing <jing.liu@spacemit.com>
2026-04-16 09:35:08 +0800

    k3:make all the screens display the desktop.
    Change-Id: I3e8d50336856b13705ba951e0d9558a4efcacd94

--- board/spacemit/k3/target_overlay/root/run_weston.sh
+++ board/spacemit/k3/target_overlay/root/run_weston.sh
@@ -2,4 +2,11 @@ export MESA_LOADER_DRIVER_OVERRIDE=pvr
 export XDG_RUNTIME_DIR=/root
 export QT_QPA_PLATFORM_PLUGIN_PATH=/usr/lib/qt/plugins/platforms
 export QT_QPA_PLATFORM=wayland
-XDG_RUNTIME_DIR=/root weston --idle-time=0 --log=/var/log/weston
+
+XDG_RUNTIME_DIR=/root weston --drm-device=card1 --idle-time=0 --log=/var/log/weston.1 &
+WESTON_PID1=$!
+
+XDG_RUNTIME_DIR=/root weston --drm-device=card2 --idle-time=0 --log=/var/log/weston.2 &
+WESTON_PID2=$!
+
+wait $WESTON_PID1 $WESTON_PID2
