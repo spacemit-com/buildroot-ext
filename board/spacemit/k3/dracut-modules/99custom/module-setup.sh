c961fa5e3d6ea415302a346b18b3135d51c7142c
bailu <lu.bai@spacemit.com>
2026-09-03 21:00:55 +0800

    k3: inject OP-TEE userland and TAs into initramfs
    dracut 99custom whitelist: add xtest/tee-supplicant/optee_example_*
    binaries and the .ta files (not ELF, need explicit inst) from
    lib/optee_armtz so the OP-TEE test stack is available in the
    initramfs.
    
    Change-Id: If3e486708efdb02877104bb9481234da414eaabe
    Signed-off-by: bailu <lu.bai@spacemit.com>
    (cherry picked from commit 189d1f23c6062cc9f63c2409eec165fcc596c02d)

--- board/spacemit/k3/dracut-modules/99custom/module-setup.sh
+++ board/spacemit/k3/dracut-modules/99custom/module-setup.sh
@@ -17,6 +17,21 @@ install() {
         ping netstat rpcinfo vi scp ping6 ssh \
         fsck fsck.ext2 fsck.ext4 fsck.ext3 fsck.ext4dev fsck.f2fs fsck.vfat e2fsck resize2fs mount blkid
 
+    # OP-TEE userland (optee-client / optee-examples / optee-test)
+    inst_multiple -o xtest tee-supplicant \
+        optee_example_hello_world optee_example_aes optee_example_secure_storage \
+        optee_example_random
+
+    # OP-TEE TAs (.ta are not executables, inst them explicitly)
+    local ta_dir="$dracutsysrootdir/lib/optee_armtz"
+    if [ -d "$ta_dir" ]; then
+        inst_dir /lib/optee_armtz
+        local ta
+        for ta in "$ta_dir"/*.ta; do
+            [ -f "$ta" ] && inst "$ta" "/lib/optee_armtz/$(basename "$ta")"
+        done
+    fi
+
     # Copy firmware files to initramfs
     local fw="$dracutsysrootdir/../../../output/esos"
     inst_dir /lib/firmware
